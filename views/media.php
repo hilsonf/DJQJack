ba4ca658d7f12f9cc3d7270bccd94d2b0224a3de
HilsonF <hilson_francis@Hilsons-MacBook-Pro.local>
2015-09-17 00:36:33 -0400

    added styling
    added styling

--- views/media.php
+++ views/media.php
@@ -1,7 +1,12 @@
 <?php ?>
-<h1>Media</h1>
+
 
 <div class="section group">
+</br>
+</br>
+</br>
+</br>
+	<h1>Media</h1>
 
 	<div class="col span_2_of_4">
 	<h2>Latest Uploads</h2>
@@ -9,7 +14,7 @@
 
 	foreach ($results as $row) {
 
-	  echo "<div class=\"col col4\">";
+	  echo "<div class=\"col span_2_of_4\">";
 	  echo "<div id=\"" .$row['songId'] . "\" class=\"post\">";
 	  echo "<p> <h3>Song Name: </h3><span>" . $row['songname']."</span></p>";
 	  
@@ -20,11 +25,14 @@
 		}
 		else{
 			echo "<img class=\"cover\" src=\"coverUploads/". $row["songCover"] . "\" width=\"300\"/>";
-			echo "<p><audio controls><source src=\"songUploads/" . $row['songFile'] . "\" type=\"audio/mpeg\">Your browser does not support audio</audio></p>";
+			// echo "<p><audio controls><source src=\"songUploads/" . $row['songFile'] . "\" type=\"audio/mpeg\">Your browser does not support audio</audio></p>";
+			echo "<p><a class=\"listen\">Listen</a></p>";
+    		echo "<p><a id=\"download\" href=\"/?action=download&file=" . $row['songFile']. "&songId=" . $row['songId'] ."\">Download</a>" . " </p> <p clsss=\"downloadcount\"> <label>Total Downloads &nbsp;<span>". $row['downloadCount']."</span></p>";
+
 			
 		}
 		echo "</div>";
-	  		echo "</div>";
+	  	echo "</div>";
 	}
 
 
@@ -42,29 +50,47 @@
 
 <div id="gallery"> 
 
-	<h2>Gallery</h2>
+	<h1>Gallery</h1>
 	
-	<div class="col span_1_of_2">
+	<div class="col gal_1_of_4">
 	    <a href="../images/img1.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img1.jpg" alt=""/></a> <!--  Lightbox-->
 	</div>
 
-	<div class="col span_1_of_2">
+	<div class="col gal_1_of_4">
 	    <a href="../images/img2.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img2.jpg" alt="" /></a> <!--  Lightbox-->
 	</div>
 
-	<div class="col span_1_of_2">
+	<div class="col gal_1_of_4">
 	    <a href="../images/img3.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img3.jpg" alt="" /></a><!--  Lightbox-->
 	</div>
 
-	<div class="col span_1_of_2">
+	<div class="col gal_1_of_4">
 	    <a href="../images/img4.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img4.jpg" alt="" /></a> <!--  Lightbox-->
 	</div>
-	<div class="col span_1_of_2">
+	<div class="col gal_1_of_4">
 	    <a href="../images/img5.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img5.jpg" alt="" /></a> <!--  Lightbox-->
 	</div>
 
-	<div class="col span_1_of_2">
+	<div class="col gal_1_of_4">
 	     <a href="../images/img6.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img6.jpg" alt="" /></a> <!--  Lightbox-->
+	</div>
+	<div class="col gal_1_of_4">
+	     <a href="../images/img7.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img7.jpg" alt="" /></a> <!--  Lightbox-->
+	</div>
+	<div class="col gal_1_of_4">
+	     <a href="../images/img8.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img8.jpg" alt="" /></a> <!--  Lightbox-->
+	</div>
+		<div class="col gal_1_of_4">
+	     <a href="../images/img9.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img9.jpg" alt="" /></a> <!--  Lightbox-->
+	</div>
+		<div class="col gal_1_of_4">
+	     <a href="../images/img10.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img10.jpg" alt="" /></a> <!--  Lightbox-->
+	</div>
+		<div class="col gal_1_of_4">
+	     <a href="../images/img11.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img11.jpg" alt="" /></a> <!--  Lightbox-->
+	</div>
+		<div class="col gal_1_of_4">
+	     <a href="../images/img11.jpg" data-lightbox="example-set" data-title="Dj Q on the ones and twos"><img class="example-image" src="../images/img11.jpg" alt="" /></a> <!--  Lightbox-->
 	</div>
 
 	</div>
--- views/musicFeed.php
+++ views/musicFeed.php
@@ -1,69 +1,66 @@
  <script src="js/slider.js"></script>
-<h2>Top Mixes</h2>
+
 
 <div class="slider">
   <!-- Slider -->
+
+  <h1>Top Mixes</h1>
+  
   <div id="slider-one">
 
             <div>
             <img src="../images/img2.jpg" alt="" />
-
-                  <p>
-                      <a class="pop1" href="../images/img2.jpg" rel="group1" title="example title text">View Details</a>
-                      <a class="pop1" href="../images/img2.jpg" rel="group1" title="more text right here"></a>
-                  </p>
+                <p>
+                    <a class="pop1" href="../images/img2.jpg" rel="group1" title="example title text">Listen Now</a>
+                    <a class="pop1" href="../images/img2.jpg" rel="group1" title="more text right here"></a>
+                </p>
             </div>
 
             <div>
             <img src="../images/img3.jpg" alt="" />
-
-                              <p>
-                                  <a class="pop2" href="../images/img3.jpg" rel="group2" title="example title text">View Details</a>
-                                  <a class="pop2" href="../images/img3.jpg" rel="group2" title="more text right here"></a>
-                              </p>
+                <p>
+                    <a class="pop2" href="../images/img3.jpg" rel="group2" title="example title text">Listen Now</a>
+                    <a class="pop2" href="../images/img3.jpg" rel="group2" title="more text right here"></a>
+                </p>
             </div>
 
             <div>
             <img src="../images/img4.jpg" alt="" />
-
-                              <p>
-                                  <a class="pop3" href="../images/img4.jpg" rel="group3" title="example title text">View Details</a>
-                                  <a class="pop3" href="../images/img4.jpg" rel="group3" title="more text right here"></a>
-                              </p>
+                <p>
+                    <a class="pop3" href="../images/img4.jpg" rel="group3" title="example title text">Listen Now</a>
+                    <a class="pop3" href="../images/img4.jpg" rel="group3" title="more text right here"></a>
+                </p>
             </div>
 
             <div>
             <img src="images/demo-images/img5.jpg" alt="" />
-
-                              <p>
-                                  <a class="pop4" href="../images/img5.jpg" rel="group4" title="example title text">View Details</a>
-                                  <a class="pop4" href="../images/img5.jpg" rel="group4" title="more text right here"></a>
-                              </p>
+                <p>
+                    <a class="pop4" href="../images/img5.jpg" rel="group4" title="example title text">Listen Now</a>
+                    <a class="pop4" href="../images/img5.jpg" rel="group4" title="more text right here"></a>
+                </p>
             </div>
 
             <div>
             <img src="../images/img6.jpg" alt="" />
-
-                              <p>
-                                  <a class="pop5" href="../images/img6.jpg" rel="group5" title="example title text">View Details</a>
-                                  <a class="pop5" href="../images/img6.jpg" rel="group5" title="more text right here"></a>
-                              </p>
+                <p>
+                    <a class="pop5" href="../images/img6.jpg" rel="group5" title="example title text">Listen Now</a>
+                    <a class="pop5" href="../images/img6.jpg" rel="group5" title="more text right here"></a>
+                </p>
             </div>
 
             <div>
             <img src="../images/img7.jpg" alt="" />
-
-                              <p>
-                                  <a class="pop6" href="../images/img7.jpg" rel="group6" title="example title text">View Details</a>
-                                  <a class="pop6" href="../images/img7.jpg" rel="group6" title="more text right here"></a>
-                              </p>
+                <p>
+                    <a class="pop6" href="../images/img7.jpg" rel="group6" title="example title text">Listen Now</a>
+                    <a class="pop6" href="../images/img7.jpg" rel="group6" title="more text right here"></a>
+                </p>
             </div>
 
             <div>
             <img src="../images/img8.jpg" alt="" />
 
               <p>
-                  <a class="pop7" href="../images/img8.jpg" rel="group7" title="example title text">View Details</a>
+                  <a class="pop7" href="../images/img8.jpg" rel="group7" title="example title text">Listen Now</a>
                   <a class="pop7" href="../images/img8.jpg" rel="group7" title="more text right here"></a>
               </p>
             </div>
@@ -74,14 +71,14 @@
   <?php
 // echo "<div id=\"postscontainer\">";
 echo "<div class=\"section group allMix\">";
-  echo "<h2>All Mixes </h2>";
+  echo "<h1>All Mixes </h1>";
  if(count($results)=="int(0)"){
    echo "<h3>There are no songs, please post a song<h3>";
  }
 
 foreach ($results as $row) {
 
-  echo "<div class=\"col col4\">";
+  echo "<div class=\"col col6\">";
   echo "<div id=\"" .$row['songId'] . "\" class=\"post\">";
   echo "<p> <h3>Song Name: </h3><span>" . $row['songname']."</span></p>";
 
@@ -91,11 +88,14 @@ foreach ($results as $row) {
     }
     else{
         echo "<img class=\"cover\" src=\"coverUploads/". $row["songCover"] . "\" />";
-        echo "<p><audio controls><source src=\"songUploads/" . $row['songFile'] . "\" type=\"audio/mpeg\">Your browser does not support audio</audio></p>";
+        // echo "<p><audio controls><source src=\"songUploads/" . $row['songFile'] . "\" type=\"audio/mpeg\">Your browser does not support audio</audio></p>";
     }
 
-    echo "<p><a id=\"like\" href=\"/?action=like&songId=" . $row['songId']. "\">Like</a>" . "  <label>Likes <span>". $row['likeCount']."</span></label>";
-  echo "<a id=\"download\" href=\"/?action=download&file=" . $row['songFile']. "&songId=" . $row['songId'] ."\">Download</a>" . "  <label>Downloads<span>". $row['downloadCount']."</span></p>";
+    echo "<p><a class=\"listen\">Listen</a></p>";
+    echo "<p><a id=\"download\" href=\"/?action=download&file=" . $row['songFile']. "&songId=" . $row['songId'] ."\">Download</a>" . " </p> <p clsss=\"downloadcount\"> <label>Total Downloads &nbsp;<span>". $row['downloadCount']."</span></p>";
+
+  //   echo "<p><a id=\"like\" href=\"/?action=like&songId=" . $row['songId']. "\">Like</a>" . "  <label>Likes <span>". $row['likeCount']."</span></label>";
+  // echo "<a id=\"download\" href=\"/?action=download&file=" . $row['songFile']. "&songId=" . $row['songId'] ."\">Download</a>" . "  <label>Downloads<span>". $row['downloadCount']."</span></p>";
   echo "</div>
         </div>
 
