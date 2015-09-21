6d6219daaa57815f8c3a04dbc715c14172c083f6
HilsonF <hilson_francis@Hilsons-MacBook-Pro.local>
2015-09-21 13:38:27 -0400

    added logo
    added logo

--- views/mixtape.php
+++ views/mixtape.php
@@ -1,18 +1,20 @@
 
 <?php
-	echo "<div>";
+
+	echo "<div class=\"section group\">";
+	echo "</br></br></br>";
 	echo "<h1>". $results[0]['songname'] ."</h1>";
 
-	echo "<div><img class=\"cover\" src=\"coverUploads/". $results[0]["songCover"] . "\" /></div>";
+	echo "<div class=\"col span_1_of_2\"><img class=\"cover\" src=\"coverUploads/". $results[0]["songCover"] . "\" /></div>";
 
-	echo "<p><audio controls><source src=\"songUploads/" . $results[0]['songFile'] . "\" type=\"audio/mpeg\">Your browser does not support audio</audio></p>";
+	echo "<p id=\"audio\"><audio controls><source src=\"songUploads/" . $results[0]['songFile'] . "\" type=\"audio/mpeg\">Your browser does not support audio</audio></p>";
 
 	echo "<p><a id=\"like\" href=\"/?action=like&songId=" . $results[0]['songId']. "\">Like</a>";
 
 	echo "<p><a id=\"download\" href=\"/?action=download&file=" . $results[0]['songFile']. "&songId=" . $results[0]['songId'] ."\">Download</a>";
 	echo "</div>";
 
-	echo "<div>";
+	echo "<div class=\"col span_1_of_2\">";
 	echo "<h2>Date Uploaded</h2>";
 	echo "<span>". $results[0]['created'] ."</span>";
 	echo "<h2>Total Downloads</h2>";
